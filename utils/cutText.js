8cf0ef757c29dc8d7ef618aa04cc6a3077508346
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-02-20 15:17:38 +0100

    add condition maxLength not a number

--- utils/cutText.js
+++ utils/cutText.js
@@ -2,5 +2,6 @@ module.exports = (content, maxLength) => {
   if(typeof content !== 'string') return 'Error';
   if(content.length < 1) return 'Error';
   if(content.length <= maxLength) return content;
+  if(maxLength !== typeof number) return 'Error';
   return content.substr(0, content.lastIndexOf(' ', maxLength)) + '...';
 };
