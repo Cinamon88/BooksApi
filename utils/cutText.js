5f05ad1e17f60f4257c6226b3a017947cc54248f
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-02-20 15:56:54 +0100

    maxLength lower or equal 0

--- utils/cutText.js
+++ utils/cutText.js
@@ -3,5 +3,6 @@ module.exports = (content, maxLength) => {
   if(content.length < 1) return 'Error';
   if(content.length <= maxLength) return content;
   if(maxLength !== typeof number) return 'Error';
+  if(maxLength <= 0) return 'Error';
   return content.substr(0, content.lastIndexOf(' ', maxLength)) + '...';
 };
